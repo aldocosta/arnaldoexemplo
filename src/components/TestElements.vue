0562668e35958d69a58c99ffa332557e04b0a286
Aldo Costa <aldocostasantos@hotmail.com>
2021-12-11 20:14:37 -0300

    add

--- src/components/TestElements.vue
+++ src/components/TestElements.vue
@@ -1,38 +1,54 @@
 <template>
   <div>
     <!-- directive -->
-    <div class="images" v-viewer>
-      <img v-for="src in images" :key="src" :src="src">
-    </div>
+    <!-- <div class="images" v-viewer>
+      ALGUM TEXTO
+      <img v-for="src in images" :key="src" :src="src" />
+    </div> -->
+
+    <!-- <div class="images" v-viewer>    
+      <img v-for="src in images" :key="src" :src="src" />
+    </div> -->
     <!-- component -->
+    <input type="text" v-model="qqrcoisa.name">
+    {{qqrcoisa.name}}
     <viewer :images="images">
-      <img v-for="src in images" :key="src" :src="src">
+      <div v-for="src in images" :key="src.img" >
+        <img :src="src.img" />
+        <div>
+          <label for="">{{src.txt}}</label>
+        </div>
+      </div>
+      <!-- <img v-for="src in images" :key="src.img" :src="src.img" /> -->
     </viewer>
     <!-- api -->
     <button type="button" @click="show">Click to show</button>
   </div>
 </template>
 <script>
-  import 'viewerjs/dist/viewer.css'
-  import VueViewer from 'v-viewer'
-  import Vue from 'vue'
-  Vue.use(VueViewer)
-  export default {
-    data() {
-      return {
-        images: [
-          "https://picsum.photos/200/200",
-          "https://picsum.photos/300/200",
-          "https://picsum.photos/250/200"
-        ]
-      };
+import "viewerjs/dist/viewer.css";
+import VueViewer from "v-viewer";
+import Vue from "vue";
+Vue.use(VueViewer);
+export default {
+  data() {
+    return {
+      images: [
+        // { img: "https://picsum.photos/200/200", txt: "acs" },
+        // { img: "https://picsum.photos/300/200", txt: "me myself" },
+        // { img: "https://picsum.photos/250/250", txt: "bacurau" },
+      ],
+      qqrcoisa:{
+        name:''
+      }
+    };
+  },
+  methods: {
+    show() {
+      this.$viewerApi({
+        images: this.images,
+      });
     },
-    methods: {
-      show() {
-        this.$viewerApi({
-          images: this.images,
-        })
-      },
-    },
-  }
+  },
+};
 </script>
